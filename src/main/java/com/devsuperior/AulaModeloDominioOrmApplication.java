19550ff25e0c8c65ca74023f9226efcccf069571
Leandro Arakaki <leanarakaki@gmail.com>
2023-12-03 17:55:05 -0400

    iniciando o commit

---
+++ src/main/java/com/devsuperior/AulaModeloDominioOrmApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaModeloDominioOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaModeloDominioOrmApplication.class, args);
+	}
+
+}
